d8eaa66e8e39b6743b26587971f3ac057d6156fe
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-12 18:33:21 +0500

    name.py

--- Name.py
+++ Name.py
@@ -1,6 +1,6 @@
-student_name = "Abdul Rabb"
-student_height = 5.9
-student_age = 18
+student_name = "Abdul Rabb" #for name
+student_height = 5.9 #for height
+student_age = 18 #for age
 print (student_name)
 print (student_height)
 print (student_age)
