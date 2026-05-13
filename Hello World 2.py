9cae4b6eb7e09d96229d9843a415147b3e71dff8
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-13 08:49:42 +0500

    Name.py
    I added comments and readable variable names.

--- Hello World 2.py
+++
@@ -1,2 +0,0 @@
-print("Hello, World!") #for output
-# Now program ends
--- Name.py
+++ Name.py
@@ -1,12 +1,12 @@
 student_name = "Abdul Rabb" #for name
 student_height = 5.9 #for height
 student_age = 18 #for age
-print (student_name)
-print (student_height)
-print (student_age)
-n =input ("enter your name")
-print (n)
-a= input ("enter your age;")
-print (a)
-h= input ("enter height;")
-print (h)
+print (student_name) #printing name
+print (student_height) #printing height
+print (student_age) #printing age
+name =input ("Enter your name;") #taking input
+print ("Your name is "+ name ) #output and provided input
+age= input ("Enter your age;") #taking input
+print ("Your age is "+ age)   #output and provided input
+height= input ("Enter height;") #taking input
+print ("Your height is " + height)   #output and provided input
