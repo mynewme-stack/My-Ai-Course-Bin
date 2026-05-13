3b8f43eed9fe6f02c97868f2f2eb2fca3ce1805d
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-13 17:25:35 +0500

    Update Name.py

--- Name.py
+++ Name.py
@@ -7,6 +7,6 @@ print (student_age) #printing age
 name =input ("Enter your name;") #taking input
 print ("Your name is "+ name ) #output and provided input
 age= input ("Enter your age;") #taking input
-print ("Your age is "+ age)   #output and provided input
+print ("Your age is "+ str(age))   #output and provided input
 height= input ("Enter height;") #taking input
 print ("Your height is " + height)   #output and provided input
