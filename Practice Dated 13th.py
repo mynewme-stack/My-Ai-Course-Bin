951275177138f4867243b08899bb81e2fdcc2855
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-13 18:24:32 +0500

    Create Practice Dated 13th.py

---
+++ Practice Dated 13th.py
@@ -0,0 +1,15 @@
+product_id = int(45)
+price = 5000
+tax = 50
+total = price + tax
+print(product_id)
+print(price)
+print(tax)
+print(total)
+discount = float(5.5)
+net_total = total - discount
+print(discount)
+print(net_total)
+product_warehouse = "WAREHOUSE ABC INC"
+product_title = "IPhone 17 Pro Max"
+product_sale =500000
