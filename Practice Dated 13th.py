cc5e8e4fc83b07be2c0a99f8b048593c0e6e4937
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-14 10:45:27 +0500

    Update Practice Dated 13th.py

--- Practice Dated 13th.py
+++ Practice Dated 13th.py
@@ -1,5 +1,5 @@
 product_id = int(45)
-price = 5000
+price = int(5000)
 tax = 50
 total = price + tax
 print(product_id)
@@ -13,3 +13,8 @@ print(net_total)
 product_warehouse = "WAREHOUSE ABC INC"
 product_title = "IPhone 17 Pro Max"
 product_sale =500000
+discount = 3
+percentage= 3 / net_total * 100
+print (product_warehouse+product_title)
+print (product_warehouse+str(price))
+print (net_total - total)
