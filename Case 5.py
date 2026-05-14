fc2d578962126c4e8033850146f0fc903b362db3
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-14 10:53:59 +0500

    Create Case 5.py

---
+++ Case 5.py
@@ -0,0 +1,50 @@
+# Specific Data Type
+strVar = str("Hello World - Richard") #Best Method
+print(type(strVar)) #only type output
+
+strVar2="Hello World - Richard"
+print(type(strVar2))
+
+
+#---------------------- int
+
+intVar = int(50) #Best Method
+print(type(intVar)) #only type output
+
+
+intVar2=50
+print(type(intVar2)) 
+
+
+
+#---------------------- float
+
+floatVar = float(50.3) #Best Method
+print(type(floatVar)) #only type output
+
+
+floatVar2=50.3
+print(type(floatVar2))
+
+#---------------------
+
+
+#---------------------- float = with an "e" to indicate the power of 10
+
+floatEVar = float(35e3) #Best Method
+print(type(floatEVar)) #only type output
+
+
+floatEVar2=35e3
+print(type(floatEVar2))
+
+#---------------------
+
+#---------------------- Complex = numbers are written with a "j" as the imaginary part
+ComplexVar = complex(3+5j) #Best Method
+print(type(ComplexVar)) #only type output
+
+
+ComplexVar2=3+5j
+print(type(ComplexVar2))
+
