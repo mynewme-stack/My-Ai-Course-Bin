4356dfe1c49dde32ff112ec1fb5b8d951d2bcc4e
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-18 17:05:55 +0500

    Update Case 8.py

--- Case 8.py
+++ Case 8.py
@@ -1,22 +1,15 @@
 # logical AND
-print(True and True)     
-print(True and False)    
-print(False and True)
-print(False and False)
+print(True and True)     # True
+print(True and False)    # False
 
 # logical OR
-print(True or True)     
-print(True or False)    
-print(False or True)
-print(False or False)
+print(True or False)     # True
 
 # logical NOT
-print(not True)   
-print(not False)       
+print(not True)          # False
 
 # operator
 a = 5
 b = 6
-print((a > 2) and (b >= 6))
-print((a > 2) or (b >= 5))
-print(not(a == 2) and not(b == 5)) # My Attempt
+
+print((a > 2) and (b >= 6))    # True
