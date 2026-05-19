8efdc27e8ce7ab09105b47e2712177c7e3dbf2a6
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-19 18:44:26 +0500

    Hello

---
+++ Practice for Week 1.py
@@ -0,0 +1,26 @@
+book_title = "Think and Grow Rich"
+book_price = 45.5
+author = "Napoleon Hill"
+book_year = 2025
+print( "Book Title: ",book_title, type(book_title))
+print("Book Price: ",book_price, type(book_price))
+print("Author: ",author, type(author))
+print("Year: ",book_year, type(book_year))
+percentage = (book_price * 10) / 100
+print(percentage)
+book_price = book_price + 5
+book_price += 5
+print(book_price)
+book_description = book_title + " is execelent book. I have read it multiple times. "
+print(book_description)
+print(book_title[15::])
+print(len(book_title))
+for i in book_title:
+    print(i)
+age = int(input("Please enter your age: "))
+if age > 10:
+    print("Greater!")
+else : 
+    print()
+
+
