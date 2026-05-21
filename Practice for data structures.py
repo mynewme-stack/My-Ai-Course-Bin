4869ed0efabb5a42ecdea851f03f9f178bd963f3
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-21 09:10:53 +0500

    Case 16

---
+++ Practice for data structures.py
@@ -0,0 +1,22 @@
+products_list = ["Vivo Y28","Blue", 50.0 , 50000]
+print(products_list)
+print(type(products_list))
+print(products_list[1])
+for x in products_list:
+    print(x)
+products_tuple = ("Vivo Y28","Blue", 50.0 , 50000)
+print(products_tuple)
+print(type(products_tuple))
+print(products_tuple[0])
+print(products_tuple[1])
+print(products_tuple[2])
+print(products_tuple[3])
+for x in products_tuple:
+    print(x)
+sett = {"Vivo Y28","Blue", 50.0 , 50000}
+for i in sett:
+    print(i)
+dictt = {"Title:""Vivo Y28","Color:""Blue","Camera:" "50.0", "Price: ""50000",}
+for i in dictt[]:
+    print(i)
+
