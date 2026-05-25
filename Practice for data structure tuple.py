c0a070898bba647cd75faaa02326f6b78a7fea2c
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-25 17:36:29 +0500

    g

---
+++ Practice for data structure tuple.py
@@ -0,0 +1,18 @@
+mobile_list = ("Oppo f19", 50, 35000.05, "Oppo", True, "It is a phone.")
+print(mobile_list)
+print(type(mobile_list))
+for i in mobile_list:
+    print(i)
+print(mobile_list[1])
+print(type(mobile_list[1]))
+
+
+
+
+
+
+
+
+
+
+
