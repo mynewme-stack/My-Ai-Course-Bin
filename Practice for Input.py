ab08da23fca1826bf8439acdc4813d1b3590c568
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-25 18:42:59 +0500

    f

---
+++ Practice for Input.py
@@ -0,0 +1,45 @@
+print("Welcome, To my Practice.")
+name = str(input("Enter you Name: "))
+print(name)
+print(type(name))  # Type of name  
+length = len(name)       # Length of name
+print(f"Length = {length}")
+for i in name:
+    print(i)
+age = int(input("Enter your age: "))
+print(type(age))
+print(age)
+if age > 10:
+    print("Greater")
+elif (age < 6):
+    print("Smaller")
+else: 
+    print("No case.")
+height = float(input("Enter your height: "))
+print(type(height))
+print(height)
+age += 5
+print(age)
+age -= 5
+print(age)
+age *= 5
+print(age)
+age /= 5
+print(age)
+age //= 5
+print(age)
+age **= 5
+print(age)
+if age > 10:
+    print("Greater")
+elif (age < 6):
+    print("Smaller")
+else: 
+    print("No case.")
+
+
+
+
+
+
+
