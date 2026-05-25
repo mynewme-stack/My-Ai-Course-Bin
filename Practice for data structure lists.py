ab2b00344ffd4f73202674f38c4c56192984f39f
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-05-25 17:23:55 +0500

    d

---
+++ Practice for data structure lists.py
@@ -0,0 +1,21 @@
+mobile_list = ["Oppo f19", 50, 35000.05, "Oppo", True, "It is a phone."]
+print(mobile_list)
+print(type(mobile_list))
+for i in mobile_list:
+    print(i)
+mobile_list.append("Taiwan")
+print(mobile_list)
+mobile_list.insert(2, 4.5)
+print(mobile_list)
+print(mobile_list[1])
+print(type(mobile_list[1]))
+mobile_list.remove(35000.05)
+print(mobile_list)
+mobile_list.pop(0)
+print(mobile_list)
+
+
+
+
+
+
