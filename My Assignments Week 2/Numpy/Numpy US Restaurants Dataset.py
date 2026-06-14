43ee067ab9b8993d12cc0b9190547e9d0e26d564
catherinewilltink-stack <catherinewilltink@gmail.com>
2026-06-14 10:35:23 +0500

    kj

--- My Assignments Week 2/Numpy/Numpy US Restaurants Dataset.py
+++ My Assignments Week 2/Numpy/Numpy US Restaurants Dataset.py
@@ -1,67 +1,97 @@
 import numpy as np
-latitude,longitude = np.genfromtxt("Class-data-science/FastFoodRestaurants.csv", delimiter=",",usecols=(4,5),unpack=True,dtype= float,skip_header=1,invalid_raise=False)
+latitude,longitude = np.genfromtxt("Datasets/FastFoodRestaurants.csv", delimiter=",",usecols=(4,5),unpack=True,dtype= float,skip_header=1,invalid_raise=False)
 
 # Displaying Output
 
-print(latitude)
-print(longitude)
+print(f'Latitude:\n{latitude}')
+print(f'Longitude:\n{longitude}')
+
+# Arithmetic
+
+print("Addition of Longitude and Latitude: ", longitude + latitude)
+print("Subtraction of Longitude and Latitude: ", longitude - latitude)
+print("Multiplication of Longitude and Latitude: ", longitude * latitude)
+print("Division of Longitude and Latitude: ", longitude / latitude)
+print("Floor Division of Longitude and Latitude: ", longitude // latitude)
+
+# Math Operations
+
+print("Square root of Longitude = ", np.sqrt(longitude))
+print("Square of Longitude = ", np.square(longitude))
+print("Power of Longitude = ", np.power(longitude,0.2))
+print("Absolute Value of Longitude = ", np.abs(longitude))
 
 # Basic Statistics
 
-print("Maximium Longitude = ", max(longitude))
-print("Minimium Latitude = ", min(latitude))
+print("Maximum Longitude = ", max(longitude))
+print("Minimum Latitude = ", min(latitude))
 print("Mean of Longitude = ", np.mean(longitude))
-print("Average of longitude = ", np.average(longitude))
-print("Standard longitude = ", np.std(longitude))
-print("Median = " ,np.median(longitude))
+print("Average of Longitude = ", np.average(longitude))
+print("Standard Longitude = ", np.std(longitude))
+print("Median of Longitude = " ,np.median(longitude))
 print("Percentile of 50% = ", np.percentile(longitude, 50))
 print("Percentile of 25% = ", np.percentile(longitude, 25))
 
-# Math Operations
-print("Square root of longitude = ", np.sqrt(longitude))
-print("Square of longitude = ", np.square(longitude))
-print("Power of longitude = ", np.power(longitude,0.5))
-print("Absolute Value of longitude = ", np.abs(longitude))
-# Arithmetic
-print("Addition of longitude and latitude: ", longitude + latitude)
-print("Subtraction of longitude and latitude: ", longitude - latitude)
-print("Multiplication of longitude and latitude: ", longitude * latitude)
-print("Division of longitude and latitude: ", longitude / latitude)
-print("Floor Division of longitude and latitude: ", longitude // latitude)
 # Trignometry
+
 latpi = np.pi * latitude
+
 print("Latitude multiply Pie = ", latpi)
 print("Sine = ", np.sin(latpi))
 print("Cosine = ", np.cos(latpi))
 print("Tangent = ", np.tan(latpi))
-normal = max(latitude)
-print("Exponential values = ", np.exp(np.clip(latitude, None, 1)))
+
+# Hyperbolic
+
+print("Sinh = ", np.sinh(latitude))
+print("Cosh = ", np.cosh(latitude))
+print("Tanh = ", np.tanh(latitude))
+
+# Inverse hyperbolic
+
+latpi_ratio = latpi/(latpi+1)                                # For taking value between 0 and 1 for tangent
+print("Inverse Sinh = ", np.arcsinh(latitude))
+print("Inverse Cosh = ", np.arccosh(latitude))
+print("Inverse Tanh = ", np.arctanh(latpi_ratio))
+
+# Exponential Value
+
+print("Exponential values = ", np.exp(latpi))
+
 # Logarithm
+
 print("Log :", np.log(latpi))
 print("Base 10 Log :", np.log10(latpi))
-# Hyperbolic
-print("Sinh = ", np.sinh(latpi))
-print("Cosh = ", np.cosh(latpi))
-print("Tanh = ", np.tanh(latpi))
-# Inverse hyperbolic
-print("Inverse Sinh = ", np.arcsinh(latpi))
-print("Inverse Cosh = ", np.arccosh(latpi))
-print("Inverse Tanh = ", np.arctanh(latpi))
+
+# 2nd Dimension
+
 longlat_2d= np.array([latitude, longitude])
 print("2D array: ",longlat_2d)
-print("Dimesion of Longitude and Latitude: ",longlat_2d.ndim) 
-print("Size : ", longlat_2d.size)
-print("Shape : ", longlat_2d.shape)
-print("Data Type : ", longlat_2d.dtype)
-# Slicing
+
+# Information of dimension
+
+print("Dimension of Longitude and Latitude: ",longlat_2d.ndim) 
+print("Size: ", longlat_2d.size)
+print("Shape: ", longlat_2d.shape)
+print("Data Type: ", longlat_2d.dtype)
+
+# Slice
+
 longlat_2d_slice = longlat_2d[1:2 , 2:5]
-print("Sliced : ", longlat_2d_slice)
-# IN dexing
+print("Sliced: ", longlat_2d_slice)
+
+# Indexing
 longlat_2d_item = longlat_2d[1,0]
-print("One item : ",longlat_2d_item)
-for i in np.nditer(longlat_2d):
+print("One item: ",longlat_2d_item)
+
+# Ways of showing output
+
+for i in np.nditer(longlat_2d):                          # Without index
     print(i)
-for index, i in np.ndenumerate(longlat_2d):
+for index, i in np.ndenumerate(longlat_2d):             # With index
     print(index,i)
+
+# Reshape
+
 longlat_2d_re = np.reshape(longlat_2d, (2, -1 ))
-print(longlat_2d_re)
+print('Reshaped:\n',longlat_2d_re)
--- My Assignments Week 2/Numpy/Numpy US Start-up Funding Dataset.py
+++ My Assignments Week 2/Numpy/Numpy US Start-up Funding Dataset.py
@@ -1,62 +1,101 @@
 import numpy as np
-investment,valuation = np.genfromtxt("Class-data-science/startup_growth_investment_data.csv", delimiter=",", usecols=(3,4), dtype=float, skip_header=1, unpack= True, encoding= None)
-print(investment)
-print(valuation)
-print("Maximium Investment = ", max(investment))
-print("Minimium Valuation = ", min(valuation))
-print("Maximium Investment = ", max(investment))
-print("Minimium Valuation = ", min(valuation))
+investment,valuation = np.genfromtxt("Datasets/startup_growth_investment_data.csv", delimiter=",", usecols=(3,4), dtype=float, skip_header=1, unpack= True, encoding= None)
+
+# Output
+
+print(f'Investment:\n{investment}')
+print(f'Valuation:\n{valuation}')
+
+# Math Operations
+
+print("Square root of Investment = ", np.sqrt(investment))
+print("Square of Investment = ", np.square(investment))
+print("Power of Investment = ", np.power(investment,0.4))
+print("Absolute Value of Investment = ", np.abs(investment))
+
+# Arithmetic
+
+print("Addition of Investment and Valuation: ", investment + valuation)
+print("Subtraction of Investment and Valuation: ",investment - valuation)
+print("Multiplication of Investment and Valuation: ", investment * valuation)
+print("Division of Investment and Valuation: ",investment / valuation)
+print("Floor Division of Investment and Valuation: ", investment // valuation)
+
+# Trignometry
+
+valpi = np.pi * valuation
+print("Valuation multiply Pie = ", valpi)
+
+print("Sine = ", np.sin(valpi))
+print("Cosine = ", np.cos(valpi))
+print("Tangent = ", np.tan(valpi))
+
+# Hyperbolic
+
+val_ratio = valpi/(valpi+1)
+
+print("Sinh = ", np.sinh(valuation))
+print("Cosh = ", np.cosh(valuation))
+print("Tanh = ", np.tanh(valuation))
+
+# Inverse hyperbolic
+
+print("Inverse Sinh = ", np.arcsinh(valuation))
+print("Inverse Cosh = ", np.arccosh(valuation))
+print("Inverse Tanh = ", np.arctanh(val_ratio))
+
+# Exponential Value
+
+print("Exponential values = ", np.exp(val_ratio))
+
+# Logarithm
+
+print("Log :", np.log(investment))
+print("Base 10 Log :", np.log10(investment))
+
+# 2nd Dimension
+
+invval_2d= np.array([investment, valuation])
+print("2D array: ",invval_2d)
+
+# Qualities of 2nd Dimension Array
+
+print("Dimension of Investment and Valuation: ",invval_2d.ndim) 
+print("Size : ", invval_2d.size)
+print("Shape : ", invval_2d.shape)
+print("Data Type : ", invval_2d.dtype)
+
+# Slicing
+
+invval_2d_slice = invval_2d[1:2 , 2:5]
+print("Sliced : ", invval_2d_slice)
+
+# Indexing
+
+invval_2d_item = invval_2d[1,0]
+print("One item : ",invval_2d_item)
+
+# Showing data
+
+for i in np.nditer(invval_2d):                # Without index
+    print(i)
+for index, i in np.ndenumerate(invval_2d):      # With index
+    print(index,i)
+
+# Statistics
+
+print("Maximum Investment = ", max(investment))
+print("Minimum Investment = ", min(investment))
+print("Maximum Valuation = ", max(valuation))
+print("Minimum Valuation = ", min(valuation))
 print("Mean of Investment = ", np.mean(investment))
 print("Average of Investment = ", np.average(investment))
 print("Standard Investment = ", np.std(investment))
 print("Median = " ,np.median(investment))
-print("Percentile of 50% = ", np.percentile(investment, 50))
-print("Percentile of 25% = ", np.percentile(investment, 25))
-# Math Operations
-print("Square root of Investment = ", np.sqrt(investment))
-print("Square of investment = ", np.square(investment))
-print("Power of investment = ", np.power(investment,0.5))
-print("Absolute Value of investment = ", np.abs(investment))
-# Arithmetic
-print("Addition of investment and valuation: ", investment+valuation)
-print("Subtraction of investment and valuation: ",investment - valuation)
-print("Multiplication of investment and valuation: ", investment * valuation)
-print("Division of investment and valuation: ",investment / valuation)
-print("Floor Division of investment and valuation: ", investment // valuation)
-# Trignometry
-valpi = np.pi * valuation
-print("Latitude multiply Pie = ", valpi)
-print("Sine = ", np.sin(valpi))
-print("Cosine = ", np.cos(valpi))
-print("Tangent = ", np.tan(valpi))
-normal = max(investment)
-print("Exponential values = ", np.exp(np.clip(investment, None, 1)))
-# Logarithm
-print("Log :", np.log(investment))
-print("Base 10 Log :", np.log10(investment))
-# Hyperbolic
-print("Sinh = ", np.sinh(valpi))
-print("Cosh = ", np.cosh(valpi))
-print("Tanh = ", np.tanh(valpi))
-# Inverse hyperbolic
-print("Inverse Sinh = ", np.arcsinh(valpi))
-print("Inverse Cosh = ", np.arccosh(valpi))
-print("Inverse Tanh = ", np.arctanh(valpi))
-invval_2d= np.array([investment, valuation])
-print("2D array: ",invval_2d)
-print("Dimesion of Longitude and Latitude: ",invval_2d.ndim) 
-print("Size : ", invval_2d.size)
-print("Shape : ", invval_2d.shape)
-print("Data Type : ", invval_2d.dtype)
-# Slicing
-invval_2d_slice = invval_2d[1:2 , 2:5]
-print("Sliced : ", invval_2d_slice)
-# IN dexing
-invval_2d_item = invval_2d[1,0]
-print("One item : ",invval_2d_item)
-for i in np.nditer(invval_2d):
-    print(i)
-for index, i in np.ndenumerate(invval_2d):
-    print(index,i)
+print("Percentile of 70% = ", np.percentile(investment, 70))
+print("Percentile of 15% = ", np.percentile(investment, 15))
+
+# Reshaped
+
 invval_2d_re = np.reshape(invval_2d, (2, -1 ))
-print(invval_2d_re)
+print('Reshaped:\n',invval_2d_re)
